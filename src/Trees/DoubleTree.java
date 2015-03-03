af01d24b6ac998ec3cbc9daa874906f0a82e5e58
Poorvankbhatia <puravbhatia9@gmail.com>
2015-03-03 16:08:52 +0530

    double tree

---
+++ src/Trees/DoubleTree.java
@@ -0,0 +1,42 @@
+package Trees;
+
+public class DoubleTree {
+    
+    public static void main(String[] args) {
+        
+        Node root = Input.treeInput();
+        System.out.println("In order before - ");
+        traversal.in_Order(root);
+        System.out.println("In order after - ");
+        traversal.in_Order(conversionDT(root));
+        
+        
+        
+    }
+    
+    private static Node conversionDT(Node root) {
+        
+        if(root == null) {
+            return null;
+        }
+        
+        Node tmp = null;
+        
+
+        if(root.right!=null) {
+            root.right = conversionDT(root.right);
+        }
+        if(root.left!=null) {
+            root.left = conversionDT(root.left);
+        }
+        tmp = new Node(root.info);  
+        tmp.left = root.left;
+        root.left = tmp;
+
+        
+        
+        return root;
+        
+    }
+    
+}
