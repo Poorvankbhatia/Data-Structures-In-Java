e13357d94abfbcbe7e124e6e1e5aae850fe7d642
Poorvankbhatia <puravbhatia9@gmail.com>
2015-03-10 10:50:36 +0530

    sumtree

---
+++ src/Trees/ConvertToSumTree.java
@@ -0,0 +1,29 @@
+package Trees;
+
+public class ConvertToSumTree {
+    
+    public static void main(String[] args) {
+        
+        Node root = Input.treeInput();
+        traversal.pre_Order(root);
+        convert(root);
+        System.out.println();
+        traversal.pre_Order(root);
+        
+    }
+    
+    private static int convert(Node root) {
+        
+        if(root == null) {
+            return 0;
+        }
+        
+        int old_val = root.info;
+        
+        root.info = convert(root.left) + convert(root.right);
+        
+        return old_val + root.info;
+        
+    }
+    
+}
