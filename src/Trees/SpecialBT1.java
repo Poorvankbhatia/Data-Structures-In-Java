30571bede66951f2c26b8ed726127c73885ece7c
Poorvankbhatia <puravbhatia9@gmail.com>
2015-03-15 01:57:02 +0530

    specialTree

---
+++ src/Trees/SpecialBT1.java
@@ -0,0 +1,21 @@
+package Trees;
+
+public class SpecialBT1 {
+    
+    public static void main(String[] args) {
+        
+        int[] inOrder = new int[]{5,10,40,30,28};
+        
+
+
+    }
+    
+    private static Node constructTree(int[] inOrder , int start , int end) {
+        
+        if(start > end) {
+            return null;
+        }
+        
+    }
+    
+}
