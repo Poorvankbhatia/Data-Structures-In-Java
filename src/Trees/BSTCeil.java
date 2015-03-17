049b4260685b63ee7c77238d406216397e1f0228
Poorvankbhatia <puravbhatia9@gmail.com>
2015-03-17 10:22:37 +0530

    floor

--- src/Trees/BSTCeil.java
+++ src/Trees/BSTCeil.java
@@ -6,8 +6,9 @@ public class BSTCeil {
         
         Node root = Input.treeInput();
         for(int i=0;i<10;i++) {
-            System.out.println("key - " + i + " ceil " + ceil(root,i));
+            System.out.println("key - " + i + " ceil " + ceil(root,i) + " floor " + floor(root,i));
         }
+        //System.out.println(floor(root,6));
         
     }
     
@@ -33,4 +34,33 @@ public class BSTCeil {
         
     }
     
+    private static int floor(Node root,int key) {
+        
+        if(root == null) {
+            return -1;
+        }
+        
+        if(root.info == key) {
+            return root.info;
+        }
+        
+        if(root.info > key) {
+            return floor(root.left,key);
+        }
+        
+        int floor = floor(root.right,key);
+        return (floor>=root.info) ? floor : root.info;
+        
+    }
+    
 }
+
+/*
+
+A) Root data is equal to key. We are done, root data is ceil value.
+
+B) Root data < key value, certainly the ceil value can’t be in left subtree. Proceed to search on right subtree as reduced problem instance.
+
+C) Root data > key value, the ceil value may be in left subtree. We may find a node with is larger data than key value in left subtree, if not the root itself will be ceil node.
+
+ */
