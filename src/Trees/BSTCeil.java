672c39d13d89b6dcadae0d7d875c602f13dc7b7f
Poorvankbhatia <puravbhatia9@gmail.com>
2015-03-18 09:00:47 +0530

    BTfromBST

--- src/Trees/BSTCeil.java
+++ src/Trees/BSTCeil.java
@@ -59,8 +59,10 @@ public class BSTCeil {
 
 A) Root data is equal to key. We are done, root data is ceil value.
 
-B) Root data < key value, certainly the ceil value can’t be in left subtree. Proceed to search on right subtree as reduced problem instance.
+B) Root data < key value, certainly the ceil value can’t be in left subtree. 
+Proceed to search on right subtree as reduced problem instance.
 
-C) Root data > key value, the ceil value may be in left subtree. We may find a node with is larger data than key value in left subtree, if not the root itself will be ceil node.
+C) Root data > key value, the ceil value may be in left subtree. 
+We may find a node with is larger data than key value in left subtree, if not the root itself will be ceil node.
 
  */
