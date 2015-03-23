7b85b0e021b4b20f4fe7e3946b8f3d598266be97
Poorvankbhatia <puravbhatia9@gmail.com>
2015-03-23 20:50:43 +0530

    NextRightNode

---
+++ src/Trees/NextRightNode.java
@@ -0,0 +1,60 @@
+package Trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by poorvank on 3/23/15.
+ */
+public class NextRightNode {
+    
+    public static void main(String[] args) {
+        
+        Node root = Input.treeInput();
+        System.out.println("Next root is - " + nextRight(root,25).info);
+        
+    }
+    
+    private static Node nextRight(Node root, int key) {
+
+        Queue<Node> nodeQueue = new LinkedList<Node>();
+        Queue<Integer> levelQueue = new LinkedList<Integer>();
+        
+        nodeQueue.add(root);
+        levelQueue.add(0);
+        
+        while (nodeQueue.size()!=0) {
+            
+
+            
+            Node current = nodeQueue.remove();
+            int level = levelQueue.remove();
+
+            
+            if(current.info == key) {
+                
+                if(nodeQueue.size()==0 || levelQueue.peek()!=level) {
+                    return null;
+                }
+                else {
+                    return nodeQueue.peek();
+                }
+                
+            }
+            if(current.left!=null) {
+                nodeQueue.add(current.left);
+                levelQueue.add(level + 1);
+            }
+            if(current.right!=null) {
+                nodeQueue.add(current.right);
+                levelQueue.add(level + 1);
+            }
+            
+            //break;
+        }
+        
+        return null;
+        
+    }
+    
+}
