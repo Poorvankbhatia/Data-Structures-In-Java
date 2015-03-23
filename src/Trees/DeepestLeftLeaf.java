72492d5fd4efc34587ad926ddd36cfd5e399d3d0
Poorvankbhatia <puravbhatia9@gmail.com>
2015-03-23 22:40:16 +0530

    Deepest left leaf

---
+++ src/Trees/DeepestLeftLeaf.java
@@ -0,0 +1,40 @@
+package Trees;
+
+/**
+ * Created by poorvank on 3/23/15.
+ */
+public class DeepestLeftLeaf {
+    
+    private static int max = 0;
+    private static Node leftNode = null; 
+    
+    public static void main(String[] args) {
+        
+        Node root = Input.treeInput();
+        deepestLeft(root,0,true);
+        System.out.println("Deepest left leaf node is - " + leftNode.info);
+        
+        
+    }
+    
+    private static void deepestLeft(Node root,int level,boolean isLeft) {
+        
+        if(root == null) {
+            return;
+        }
+        
+        if(isLeft && root.left == null && root.right == null) {
+            if(level>max) {
+                max = level;
+                leftNode = root;
+            }
+        }
+        
+        deepestLeft(root.left,level+1,true);
+        deepestLeft(root.right,level+1,false);
+        
+        
+        
+    }
+    
+}
