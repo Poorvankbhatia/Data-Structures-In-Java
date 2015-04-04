e1db69fa83b0a95bfe6fedc41b9594f9d02b1456
Poorvankbhatia <puravbhatia9@gmail.com>
2015-04-04 21:04:02 +0530

    Possible words

---
+++ src/graphs/PossibleWords.java
@@ -0,0 +1,70 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by poorvank on 4/4/15.
+ */
+public class PossibleWords {
+    
+    private static List<String> dictionary = new ArrayList<String>(){{add("GEEKS"); add("FOR"); add("QUIZ"); add("GO");}};
+    
+    public static void main(String[] args) {
+        
+        char[][] boggle = new char[][]{
+                {'G','I','Z'},
+                {'U','E','K'},
+                {'Q','S','E'}
+        };
+        
+        findWords(boggle);
+    }
+    
+    private static void findWords(char[][] boggle) {
+        
+        boolean[][] visited = new boolean[3][3];
+        
+        for (int i=0;i<3;i++) {
+            for (int j=0;j<3;j++) {
+              findWordsUtil(boggle,visited,i,j,new StringBuilder());  
+            }
+        }
+        
+    }
+    
+    private static void findWordsUtil(char[][] boggle,boolean[][] visited,int i,int j,StringBuilder sb) {
+        
+        //System.out.println(i + " " + j);
+        visited[i][j] = true;
+        sb.append(boggle[i][j]);
+        
+        if(dictionary.contains(sb.toString())) {
+            System.out.println(sb);
+        }
+        
+        for (int row = i-1;row<=i+1 && row<3;row++) {
+            for (int col=j-1;col<=j+1 && col<3;col++) {
+                if (row>=0 && col>=0 && !visited[row][col]){
+                    findWordsUtil(boggle,visited,row,col,sb);
+                }
+            }
+        }
+        
+        sb.deleteCharAt(sb.length()-1);
+        visited[i][j] = false;
+        
+    }
+    
+}
+
+
+
+/*
+
+The idea is to consider every character as a starting character and find all words starting with it. 
+All words starting from a character can be found using Depth First Traversal. 
+We do depth first traversal starting from every cell. 
+We keep track of visited cells to make sure that a cell is considered only once in a word.
+
+ */
