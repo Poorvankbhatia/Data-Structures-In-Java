4c87f6ae87b92ae644e5fd8afbf5fa3bb6efc431
Poorvankbhatia <puravbhatia9@gmail.com>
2015-04-05 09:26:27 +0530

    comments added in Bipartite

--- src/graphs/BipartiteGraph.java
+++ src/graphs/BipartiteGraph.java
@@ -67,10 +67,15 @@ public class BipartiteGraph {
 
 /*
 
+Time Complexity of the above approach is same as that Breadth First Search.
+ In above implementation is O(V^2) where V is number of vertices. 
+ If graph is represented using adjacency list, then the complexity becomes O(V+E).
+
 1.	Assign RED color to the source vertex (putting into set U).
 2.	Color all the neighbors with BLUE color (putting into set V).
 3.	Color all neighbor’s neighbor with RED color (putting into set U).
-4.	This way, assign color to all vertices such that it satisfies all the constraints of m way coloring problem where m = 2.
+4.	This way, assign color to all vertices such that it satisfies all the constraints of m way coloring
+ problem where m = 2.
 5. While assigning colors, if we find a neighbor which is colored with same color as current vertex, 
 then the graph cannot be colored with 2 vertices (or graph is not Bipartite)
 
