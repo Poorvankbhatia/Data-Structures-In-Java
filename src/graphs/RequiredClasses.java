fb288442181870afb7a7a357122a16ec29969ced
Poorvankbhatia <puravbhatia9@gmail.com>
2015-04-08 00:25:59 +0530

    refactor for directed graph

--- src/graphs/RequiredClasses.java
+++ src/graphs/RequiredClasses.java
@@ -24,15 +24,38 @@ class Vertex {
 }
 
 
+class DirectedVertex {
+    
+    public int info;
+    public int weight;
+    
+    public List<DirectedVertex> adjacentVertices;
+
+    public DirectedVertex(int info) {
+        this(info,0);
+    }
+    
+    public DirectedVertex(int info , int weight) {
+        this.info = info;
+        adjacentVertices = new ArrayList<>();
+        this.weight = weight;
+    }
+    
+    
+}
+
+
 class Graph {
 
     public int noOfVertices;
     public Vertex[] vertexArray;
+    public DirectedVertex[] directedVertexArray;
 
     public Graph(int noOfVertices) {
 
         this.noOfVertices = noOfVertices;
         this.vertexArray = new Vertex[noOfVertices];
+        this.directedVertexArray = new DirectedVertex[noOfVertices];
         
         /*
         
@@ -42,12 +65,19 @@ class Graph {
          */
         for (int i = 0; i < noOfVertices; i++) {
             vertexArray[i] = new Vertex(i);
+            directedVertexArray[i] = new DirectedVertex(i);
         }
 
     }
 
     public void addEdge(int v1, int v2) {
         vertexArray[v1].adjacentVertices.add(v2);
+    }
+
+    public void addEdge(int v1, int v2 ,int w) {
+
+        directedVertexArray[v1].adjacentVertices.add(new DirectedVertex(v2,w));
+        System.out.println("vertex  " + v1 + " to vertex " + v2 + " with weight " + directedVertexArray[v1].adjacentVertices.get(0).weight)  ;
     }
 
 }
@@ -89,6 +119,42 @@ class Input {
 
     }
 
+    public static DirectedVertex[] directedGraphInput() {
+
+        int a = 0, b = 0,c=0;
+        System.out.println("Enter number of vertices ");
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        Graph graph = new Graph(count);
+        System.out.println("Enter vertex values to create an edge between them with weight c eg a,b,c , enter s to end input");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("s")) {
+                break;
+            }
+            String[] inputSplit = input.split(",");
+            if (inputSplit.length == 3) {
+               // for (String c : input.split(",")) {
+                    a = Integer.parseInt(inputSplit[0]);
+                    b = Integer.parseInt(inputSplit[1]);
+                    c = Integer.parseInt(inputSplit[2]); 
+                    while (a >= count || b >= count) {
+                        System.out.println("Wrong Input");
+                        continue;
+                    }
+               // }
+                graph.addEdge(a, b, c);
+            } else {
+                System.out.println("Wrong Input");
+            }
+
+        }
+
+        return graph.directedVertexArray;
+
+    }
+
 }
 
 
@@ -96,9 +162,10 @@ class Stack {
 
     public final int MAX = 100;
     public int top = -1;
-    public Vertex[] stack = new Vertex[MAX];
+    public Object[] stack = new Object[MAX];
+    //public DirectedVertex[] directedStack = new DirectedVertex[MAX];
 
-    public void push(Vertex a) {
+    public void push(Object a) {
 
         top++;
         if (isFull()) {
@@ -125,10 +192,10 @@ class Stack {
         return false;
     }
 
-    public Vertex pop() {
+    public Object pop() {
 
         if (!isEmpty()) {
-            Vertex a = stack[top];
+            Object a = stack[top];
             top--;
             return a;
         }
