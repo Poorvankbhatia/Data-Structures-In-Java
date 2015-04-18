b8d72fe51934a5c9a802a2d6c37ed5f62928d787
Poorvankbhatia <puravbhatia9@gmail.com>
2015-04-18 13:06:07 +0530

    refactor

--- src/Strings/RecursivePrint.java
+++ src/Strings/RecursivePrint.java
@@ -14,58 +14,58 @@ import java.util.ArrayList;
  * Created by poorvank on 4/13/15.
  */
 public class RecursivePrint {
-    
-    private static int max=0;
-    
+
+    private static int max = 0;
+
     public static void main(String[] args) {
-        
+
         String[][] jaggedArray = {{"you", "we"},
-                            {"have", "are" },
-                            {"sleep", "eat","drink"}};
-        
+                {"have", "are"},
+                {"sleep", "eat", "drink"}};
+
         print(jaggedArray);
-     
-        
+
+
     }
-    
+
     private static void print(String[][] jaggedArray) {
-        
+
         for (String[] array : jaggedArray) {
-            if(max<array.length) {
+            if (max < array.length) {
                 max = array.length;
             }
         }
-        
-        for (int i=0;i<jaggedArray[0].length;i++) {
-            if(jaggedArray[0][i]!=null) {
-                dfsUtil(jaggedArray,0,i,new ArrayList<String>());
+
+        for (int i = 0; i < jaggedArray[0].length; i++) {
+            if (jaggedArray[0][i] != null) {
+                dfsUtil(jaggedArray, 0, i, new ArrayList<String>());
             }
         }
-        
-        
+
+
     }
-    
-    private static void dfsUtil(String[][] jaggedArray,int m,int n,ArrayList<String> arrayList){
-        
+
+    private static void dfsUtil(String[][] jaggedArray, int m, int n, ArrayList<String> arrayList) {
+
         arrayList.add(jaggedArray[m][n]);
-        
-        if(m==jaggedArray.length-1) {
+
+        if (m == jaggedArray.length - 1) {
             System.out.println(arrayList.toString());
             return;
         }
-        
-        for (int i=0;i<max;i++) {
-            
+
+        for (int i = 0; i < max; i++) {
+
             //Condition to check when array element is empty :  DRY RUN
-            if(jaggedArray[m+1].length>=i+1 && jaggedArray[m+1][i]!=null) {
-                dfsUtil(jaggedArray,m+1,i,new ArrayList<String>(arrayList));
+            if (jaggedArray[m + 1].length >= i + 1 && jaggedArray[m + 1][i] != null) {
+                dfsUtil(jaggedArray, m + 1, i, new ArrayList<String>(arrayList));
                 System.out.println();
             }
         }
 
-        
+
     }
-    
+
 }
 
 
