e95035bad6609c2e78ab7bf78799d71d20705ace
Poorvankbhatia <puravbhatia9@gmail.com>
2015-04-23 11:37:08 +0530

    k frequent words

--- src/Arrays/KFrequentWords.java
+++ src/Arrays/KFrequentWords.java
@@ -101,7 +101,7 @@ Then just traverse the array from the end, and collect the k words.
 
 We can use Trie and Min Heap to get the k most frequent words efficiently. The idea is to use Trie for searching 
 existing words adding new words efficiently. Trie also stores count of occurrences of words. A Min Heap of size k is used to keep track of k most frequent words at any point of time(Use of Min Heap is same as we used it to find k largest elements in this post).
-Trie and Min Heap are linked with each other by storing an additional field in Trie ‘indexMinHeap’ and a pointer ‘trNode’ in Min Heap. The value of ‘indexMinHeap’ is maintained as -1 for the words which are currently not in Min Heap (or currently not among the top k frequent words). For the words which are present in Min Heap, ‘indexMinHeap’ contains, index of the word in Min Heap. The pointer ‘trNode’ in Min Heap points to the leaf node corresponding to the word in Trie.
+Trie and Min Heap are linked with each other by storing an additional field in Trie ‘indexMinHeap’ and a pointer ‘trNode’ in Min Heap. The value of ‘indexMinHeap’ is maintained as -1 for the words which are currently not in Min Heap (or currently not among the top k frequent words). For the words which are present in Min Heap, ‘indexMinHeap’ contains, index of the word in Min Heap. The pointer ‘trNode’ in Min Heap points to the leaf dllNode corresponding to the word in Trie.
 
 Following is the complete process to print k most frequent words from a file.
 
@@ -109,10 +109,10 @@ Read all words one by one. For every word, insert it into Trie. Increase the cou
 
 1. The word is already present. We just increase the corresponding frequency value in min heap and call 
 minHeapify() for the index obtained by “indexMinHeap” field in Trie. When the min heap nodes are being swapped, 
-we change the corresponding minHeapIndex in the Trie. Remember each node of the min heap is also having pointer to 
-Trie leaf node.
+we change the corresponding minHeapIndex in the Trie. Remember each dllNode of the min heap is also having pointer to
+Trie leaf dllNode.
 
-2. The minHeap is not full. we will insert the new word into min heap & update the root node in the min heap node & min heap index in Trie leaf node. Now, call buildMinHeap().
+2. The minHeap is not full. we will insert the new word into min heap & update the root dllNode in the min heap dllNode & min heap index in Trie leaf dllNode. Now, call buildMinHeap().
 
 3. The min heap is full. Two sub-cases arise.
 ….3.1 The frequency of the new word inserted is less than the frequency of the word stored in the head of min heap. Do nothing.
