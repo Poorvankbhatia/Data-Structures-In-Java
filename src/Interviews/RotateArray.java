705ba20d06827caaef0a32aa106c932840769299
Poorvankbhatia <puravbhatia9@gmail.com>
2015-05-11 12:00:21 +0530

    refactor

--- src/Interviews/RotateArray.java
+++ src/Interviews/RotateArray.java
@@ -12,53 +12,53 @@ import java.util.Arrays;
  * Created by poorvank on 5/7/15.
  */
 public class RotateArray {
-    
+
     public static void main(String[] args) {
-        
-        int[][] array = new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        
+
+        int[][] array = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+
         int row = array.length;
         int col = array[0].length;
-        
+
         System.out.println("Source Array ");
-        printArray(array,row,col);
-        
-        rotateArray(array,row,col);
-        
+        printArray(array, row, col);
+
+        rotateArray(array, row, col);
+
     }
-    
-    private static void printArray (int[][] array,int row,int col) {
-        
-        for (int i=0;i<row;i++) {
-            for (int j=0;j<col;j++) {
+
+    private static void printArray(int[][] array, int row, int col) {
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
-        
+
     }
-    
-    private static void rotateArray(int[][] array,int row,int col) {
-        
+
+    private static void rotateArray(int[][] array, int row, int col) {
+
         int r = col;
         int c = row;
-        
+
         int[][] destinationArray = new int[r][c];
-        
-        for(int i=0;i<r;i++) {
-            int k=c;
-            for (int j=0;j<c;j++) {
-                System.out.println("i,j = " + i + "," + j + " && k-1,j= " + (k-1) + "," + j);
-                destinationArray[i][j] = array[k-1][i];
+
+        for (int i = 0; i < r; i++) {
+            int k = c;
+            for (int j = 0; j < c; j++) {
+                System.out.println("i,j = " + i + "," + j + " && k-1,j= " + (k - 1) + "," + j);
+                destinationArray[i][j] = array[k - 1][i];
                 k--;
             }
         }
 
         System.out.println("destination Array " + Arrays.toString(destinationArray[0]));
-        printArray(destinationArray,col,row);
-        
+        printArray(destinationArray, col, row);
+
     }
-    
+
 }
 
 
