35e410bc7ec0e8e13873d02ce1f059cf15308070
Poorvankbhatia <puravbhatia9@gmail.com>
2015-06-12 11:41:42 +0530

    Foldable trees

--- src/Trees/FoldableTree.java
+++ src/Trees/FoldableTree.java
@@ -1,3 +1,43 @@
+/*
+
+A tree can be folded if left and right subtrees of the tree are structure wise mirror image of each other.
+ An empty tree is considered as foldable.
+
+Consider the below trees:
+(a) and (b) can be folded.
+(c) and (d) cannot be folded.
+
+(a)
+       10
+     /    \
+    7      15
+     \    /
+      9  11
+
+(b)
+        10
+       /  \
+      7    15
+     /      \
+    9       11
+
+(c)
+        10
+       /  \
+      7   15
+     /    /
+    5   11
+
+(d)
+
+         10
+       /   \
+      7     15
+    /  \    /
+   9   10  12
+
+ */
+
 package Trees;
 
 public class FoldableTree {
@@ -37,3 +77,25 @@ public class FoldableTree {
     }
 
 }
+
+/*
+
+There are mainly two functions:
+
+// Checks if tree can be folded or not
+
+IsFoldable(root)
+1) If tree is empty then return true
+2) Else check if left and right subtrees are structure wise mirrors of
+    each other. Use utility function IsFoldableUtil(root->left,
+    root->right) for this.
+// Checks if n1 and n2 are mirror of each other.
+
+IsFoldableUtil(n1, n2)
+1) If both trees are empty then return true.
+2) If one of them is empty and other is not then return false.
+3) Return true if following conditions are met
+   a) n1->left is mirror of n2->right
+   b) n1->right is mirror of n2->left
+
+ */
