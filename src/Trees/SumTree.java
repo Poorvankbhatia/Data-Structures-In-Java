a8b448cabf46c13aa840bf243e91031c897d8326
Poorvankbhatia <puravbhatia9@gmail.com>
2015-06-13 11:26:10 +0530

    Sum Tree

--- src/Trees/SumTree.java
+++ src/Trees/SumTree.java
@@ -1,3 +1,11 @@
+/*
+
+A SumTree is a Binary Tree where the value of a node is equal to sum 
+of the nodes present in its left subtree and right subtree. 
+An empty tree is SumTree and sum of an empty tree can be considered as 0. A leaf node is also considered as SumTree.
+
+ */
+
 package Trees;
 
 public class SumTree {
