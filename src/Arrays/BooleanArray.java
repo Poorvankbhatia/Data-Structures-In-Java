03b37969907087afdfe811ce7e53a2058d654760
Poorvankbhatia <puravbhatia9@gmail.com>
2015-06-13 09:25:34 +0530

    Boolean Array Puzzle

---
+++ src/Arrays/BooleanArray.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Arrays;
+
+/**
+ * Created by poorvank on 6/12/15.
+ */
+public class BooleanArray {
+    
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{0,1};
+
+        arr[arr[0]] = arr[arr[0]];
+        arr[arr[1]] = arr[arr[1]];
+        
+        System.out.println(Arrays.toString(arr));
+        
+    }
+    
+}
