36dcbcaaf878807b8d5c790f5eeb5c1e175dacf9
Poorvankbhatia <puravbhatia9@gmail.com>
2015-06-25 20:28:36 +0530

    cheapest sum

--- src/Trees/RootToLeaf.java
+++ src/Trees/RootToLeaf.java
@@ -4,29 +4,43 @@ import java.util.ArrayList;
 
 public class RootToLeaf {
 
-    //private static int length;
+    private static int maxSum = Integer.MAX_VALUE;
+    private static ArrayList<Integer> finalList = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        Node root = Input.treeInput();
+        Node root = new Node(8);
+        root.left = new Node(4);
+        root.left.left = new Node(3);
+        root.left.right = new Node(1);
+        root.right = new Node(5);
+        root.right.right = new Node(11);
         ArrayList<Integer> list = new ArrayList<Integer>();
-        path(root, list);
+        path(root, list,0);
+        System.out.println("Final list is - " + finalList.toString() +  " and minimum sum is " + maxSum);
 
     }
 
-    private static void path(Node root, ArrayList<Integer> list) {
+    private static void path(Node root, ArrayList<Integer> list,int s) {
 
-        if (root != null) {
+        if(root==null) {
+            return;
+        } else {
             list.add(root.info);
+            s = s+root.info;
         }
 
-        if (root == null || (root.left == null && root.right == null)) {
+        if ((root.left == null && root.right == null)) {
             System.out.println(list);
+            if(maxSum>s) {
+                maxSum = s;
+                finalList = new ArrayList<>(list);
+            }
             return;
         }
 
-        path(root.left, new ArrayList<Integer>(list));
-        path(root.right, new ArrayList<Integer>(list));
+        path(root.left, new ArrayList<Integer>(list),s);
+        path(root.right, new ArrayList<Integer>(list),s);
 
     }
 
