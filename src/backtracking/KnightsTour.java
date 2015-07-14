d3138d6fa64a730e9ea3693e97395b2e6998fc92
Poorvankbhatia <puravbhatia9@gmail.com>
2015-07-14 11:01:03 +0530

    knights Tour refactor

--- src/backtracking/KnightsTour.java
+++ src/backtracking/KnightsTour.java
@@ -17,95 +17,91 @@ public class KnightsTour {
     /* xMove[] and yMove[] define next move of Knight.
       xMove[] is for next value of x coordinate
       yMove[] is for next value of y coordinate */
-    private static final int[] XMOVE = new int[]{  2, 1, -1, -2, -2, -1,  1,  2 };
-    private static final int[] YMOVE = new int[]{  1, 2,  2,  1, -1, -2, -2, -1 };
+    private static final int[] XMOVE = new int[]{2, 1, -1, -2, -2, -1, 1, 2};
+    private static final int[] YMOVE = new int[]{1, 2, 2, 1, -1, -2, -2, -1};
     private static final int NOTCOVERED = -1;
     private static final int N = 5;
-    
+
     public static void main(String[] args) {
-        
+
         kst();
-        
+
     }
-    
+
     private static void printSolution(int[][] sol) {
 
-        for (int i=0;i<N;i++) {
-            for (int j=0;j<N;j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 System.out.print(sol[i][j] + " ");
             }
             System.out.println();
         }
-        
+
     }
-    
+
     private static void kst() {
-        
+
         int[][] solution = new int[8][8];
-        
-        for (int i=0;i<N;i++) {
-            for (int j=0;j<N;j++) {
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 solution[i][j] = NOTCOVERED;
             }
         }
-        
+
         solution[3][3] = 0;
-        
-        if(!kstUtil(solution,1,4,4)) {
+
+        if (!kstUtil(solution, 1, 4, 4)) {
             System.out.println("Solution does not exists");
-        }
-        else {
+        } else {
             System.out.println("Solution found is - \n");
             printSolution(solution);
         }
-        
-        
-        
+
+
     }
-    
-    private static boolean kstUtil(int[][] sol,int move,int x,int y) {
-        
-        int nextX=0,nextY=0;
-        if(move==(N*N)) {
+
+    private static boolean kstUtil(int[][] sol, int move, int x, int y) {
+
+        int nextX = 0, nextY = 0;
+        if (move == (N * N)) {
             return true;
         }
-        
-        for (int k=0;k<8;k++) {
 
-            
-            nextX = x+XMOVE[k];
-            nextY = y+YMOVE[k];
-            
-            if(isSafe(sol,nextX,nextY)) {
-                
+        for (int k = 0; k < 8; k++) {
+
+
+            nextX = x + XMOVE[k];
+            nextY = y + YMOVE[k];
+
+            if (isSafe(sol, nextX, nextY)) {
+
                 sol[nextX][nextY] = move;
-                if(kstUtil(sol, move + 1, nextX, nextY)) {
+                if (kstUtil(sol, move + 1, nextX, nextY)) {
                     return true;
-                }
-                else {
-                   // System.out.println("Did not work for " + nextX + " & " + nextY);
+                } else {
+                    // System.out.println("Did not work for " + nextX + " & " + nextY);
                     sol[nextX][nextY] = NOTCOVERED;
                 }
-                
-                
+
+
             }
-            
-            
+
+
         }
-        
+
         return false;
-        
+
     }
-    
-    
-    private static boolean isSafe(int[][] sol,int x,int y) {
+
+
+    private static boolean isSafe(int[][] sol, int x, int y) {
 
         return x >= 0 && x < N && y >= 0 && y < N && sol[x][y] == NOTCOVERED;
-        
+
     }
-    
-    
-    
+
+
 }
 
 /*
