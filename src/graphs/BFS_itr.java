0b8a9790f02f00fa1db0ea338e505a4bf8f468d7
Poorvankbhatia <puravbhatia9@gmail.com>
2015-08-13 11:55:57 +0530

    BFS And DFS

--- src/graphs/BFS_itr.java
+++ src/graphs/BFS_itr.java
@@ -51,4 +51,15 @@ public class BFS_itr {
 Note that the above code traverses only the vertices reachable
  from a given source vertex. All the vertices may not be reachable from a given vertex
 
+
+In a breadth first search, you start at the root node, and then scan each node in the
+ first level starting from the leftmost node, moving towards the right.
+ Then you continue scanning the second level (starting from the left) and the third level,
+ and so on until you’ve scanned all the nodes, or until you find the actual node that you
+ were searching for. In a BFS, when traversing one level, we need some way of knowing which nodes
+ to traverse once we get to the next level. The way this is done is by storing the pointers to a level’s
+  child nodes while searching that level. The pointers are stored in FIFO (First-In-First-Out) queue.
+   This, in turn, means that BFS uses a large amount of memory because we have to store the pointers.
+
  */
+
--- src/graphs/DFS_itr.java
+++ src/graphs/DFS_itr.java
@@ -41,17 +41,3 @@ public class DFS_itr {
     }
 
 }
-
-
-/*
-
-In a breadth first search, you start at the root node, and then scan each node in the
- first level starting from the leftmost node, moving towards the right. 
- Then you continue scanning the second level (starting from the left) and the third level, 
- and so on until you’ve scanned all the nodes, or until you find the actual node that you 
- were searching for. In a BFS, when traversing one level, we need some way of knowing which nodes 
- to traverse once we get to the next level. The way this is done is by storing the pointers to a level’s
-  child nodes while searching that level. The pointers are stored in FIFO (First-In-First-Out) queue.
-   This, in turn, means that BFS uses a large amount of memory because we have to store the pointers.
-
- */
