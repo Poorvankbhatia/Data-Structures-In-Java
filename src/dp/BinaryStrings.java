384dcf7db69cbe444c75411598c0d21666e42722
Poorvankbhatia <puravbhatia9@gmail.com>
2015-08-14 19:04:41 +0530

    Binary Strings Question Added

--- src/dp/BinaryStrings.java
+++ src/dp/BinaryStrings.java
@@ -1,3 +1,21 @@
+/*
+
+Count number of binary strings without consecutive 1’s
+Given a positive integer N, count all possible distinct binary strings of length N such that 
+there are no consecutive 1’s.
+
+Examples:
+
+Input:  N = 2
+Output: 3
+// The 3 strings are 00, 01, 10
+
+Input: N = 3
+Output: 5
+// The 5 strings are 000, 001, 010, 100, 101
+
+ */
+
 package dp;
 
 /**
