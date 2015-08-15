0ea7c506b031d1daae816183f7fb1ddd26f66113
Poorvankbhatia <puravbhatia9@gmail.com>
2015-08-15 14:00:00 +0530

    Bitonic Refactor

--- src/dp/LongestBitonicSubSeq.java
+++ src/dp/LongestBitonicSubSeq.java
@@ -1,3 +1,24 @@
+/*
+
+Given an array arr[0 … n-1] containing n positive integers, a subsequence of arr[] is called Bitonic 
+if it is first increasing, then decreasing. Write a function that takes an array as argument and returns 
+the length of the longest bitonic subsequence.
+A sequence, sorted in increasing order is considered Bitonic with the decreasing part as empty. Similarly, 
+decreasing order sequence is considered Bitonic with the increasing part as empty.
+
+Examples:
+
+Input arr[] = {1, 11, 2, 10, 4, 5, 2, 1};
+Output: 6 (A Longest Bitonic Subsequence of length 6 is 1, 2, 10, 4, 2, 1)
+
+Input arr[] = {12, 11, 40, 5, 3, 1}
+Output: 5 (A Longest Bitonic Subsequence of length 5 is 12, 11, 5, 3, 1)
+
+Input arr[] = {80, 60, 30, 40, 20, 10}
+Output: 5 (A Longest Bitonic Subsequence of length 5 is 80, 60, 30, 20, 10)
+
+ */
+
 package dp;
 
 import java.util.Arrays;
@@ -60,3 +81,14 @@ public class LongestBitonicSubSeq {
     }
 
 }
+
+
+/*
+
+This problem is a variation of standard Longest Increasing Subsequence (LIS) problem.
+ Let the input array be arr[] of length n. We need to construct two arrays lis[] and lds[] using Dynamic Programming 
+ solution of LIS problem. lis[i] stores the length of the Longest Increasing subsequence ending with arr[i].
+  lds[i] stores the length of the longest Decreasing subsequence starting from arr[i]. Finally, we need to 
+  return the max value of lis[i] + lds[i] – 1 where i is from 0 to n-1.
+
+ */
