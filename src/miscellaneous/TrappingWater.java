3ea6f85c7d64e0cfff481c74da7190beaa0f46de
Poorvankbhatia <puravbhatia9@gmail.com>
2015-08-18 08:43:51 +0530

    Trapping Water

--- src/miscellaneous/TrappingWater.java
+++ src/miscellaneous/TrappingWater.java
@@ -1,7 +1,83 @@
+/*
+
+Trapping Rain Water
+Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+compute how much water it is able to trap after raining.
+
+Examples:
+
+Input: arr[]   = {2, 0, 2}
+Output: 2
+Structure is like below
+| |
+|_|
+We can trap 2 units of water in the middle gap.
+
+Input: arr[]   = {3, 0, 0, 2, 0, 4}
+Output: 10
+Structure is like below
+     |
+|    |
+|  | |
+|__|_| 
+We can trap "3*2 units" of water between 3 an 2,
+"1 unit" on top of bar 2 and "3 units" between 2 
+and 4.  See below diagram also.
+
+Input: arr[] = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+Output: 6
+       | 
+   |   || |
+_|_||_||||||
+Trap "1 unit" between first 1 and 2, "4 units" between
+first 2 and 3 and "1 unit" between second last 1 and last 2 
+
+ */
+
 package miscellaneous;
 
 /**
  * Created by poorvank on 8/17/15.
  */
 public class TrappingWater {
+    
+    public static void main(String[] args) {
+        
+        int[] arr = new int[]{3, 0, 0, 2, 0, 4};
+        
+        int n = arr.length;
+        
+        int[] left = new int[n];
+        int[] right = new int[n];
+        
+        left[0] = arr[0];
+        for (int i=1;i<n;i++) {
+            left[i] = Math.max(arr[i],left[i-1]);
+        }
+        
+        right[n-1]=arr[n-1];
+        for (int i=n-2;i>=0;i--) {
+            right[i] = Math.max(right[i+1],arr[i]);
+        }
+        
+        int water = 0;
+        
+        for (int i=0;i<n;i++) {
+            
+            water+= Math.min(left[i],right[i])-arr[i];
+            
+        }
+        
+        System.out.println("Maximum Water units = " + water);
+        
+    }
+    
 }
+
+
+/*
+
+An Efficient Solution is to prre-compute highest bar on left and right of every bar in O(n) time. 
+Then use these pre-computed values to find the amount of water in every array element. 
+
+ */
