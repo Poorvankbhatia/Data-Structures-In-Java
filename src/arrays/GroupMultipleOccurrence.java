29c53536c8005792374849bdfc35e1b0d0dccaac
Poorvankbhatia <puravbhatia9@gmail.com>
2015-08-29 17:35:38 +0530

    Group Multiple Occurrence

--- src/arrays/GroupMultipleOccurrence.java
+++ src/arrays/GroupMultipleOccurrence.java
@@ -1,3 +1,18 @@
+/*
+
+Given an unsorted array with repetitions, the task is to group multiple occurrence of individual elements. 
+The grouping should happen in a way that the order of first occurrences of all elements is maintained.
+
+Examples:
+
+Input: arr[] = {5, 3, 5, 1, 3, 3}
+Output:        {5, 5, 3, 3, 3, 1}
+
+Input: arr[] = {4, 6, 9, 2, 3, 4, 9, 6, 10, 4}
+Output:        {4, 4, 4, 6, 6, 9, 9, 2, 3, 10}
+
+ */
+
 package arrays;
 
 import java.util.HashMap;
