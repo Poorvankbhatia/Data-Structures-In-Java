88b4c5305029ab847282af88fd561923689645bb
Poorvankbhatia <puravbhatia9@gmail.com>
2015-08-29 17:00:21 +0530

    Find K CLosest

--- src/arrays/binarysearching/FindKClosestElements.java
+++ src/arrays/binarysearching/FindKClosestElements.java
@@ -5,8 +5,7 @@ Given a sorted array arr[] and a value X, find the k closest elements to X in ar
 Examples:
 
 Input: K = 4, X = 35
-       arr[] = {12, 16, 22, 30, 35, 39, 42, 
-               45, 48, 50, 53, 55, 56}
+       arr[] = {12, 16, 22, 30, 35, 39, 42, 45, 48, 50, 53, 55, 56}
 Output: 30 39 42 45
 Note that if the element is present in array, then it should not be in output, only the other closest elements are required.
 
