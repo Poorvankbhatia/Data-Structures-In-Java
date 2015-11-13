94e299fd5ac03b7628ca4604a32c966dd8c3d4d2
Poorvankbhatia <puravbhatia9@gmail.com>
2015-11-13 10:06:08 +0530

    FloydWarshall Agorithm

---
+++ src/graphs/FloydWarshall.java
@@ -0,0 +1,97 @@
+/*
+
+The Floyd Warshall Algorithm is for solving the All Pairs Shortest Path problem. 
+The problem is to find shortest distances between every pair of vertices in a given edge weighted directed Graph.
+
+Example:
+
+Input:
+       graph[][] = { {0,   5,  INF, 10},
+                    {INF,  0,  3,  INF},
+                    {INF, INF, 0,   1},
+                    {INF, INF, INF, 0} }
+which represents the following graph
+             10
+       (0)------->(3)
+        |         /|\
+      5 |          |
+        |          | 1
+       \|/         |
+       (1)------->(2)
+            3       
+Note that the value of graph[i][j] is 0 if i is equal to j 
+And graph[i][j] is INF (infinite) if there is no edge from vertex i to j.
+
+Output:
+Shortest distance matrix
+      0      5      8      9
+    INF      0      3      4
+    INF    INF      0      1
+    INF    INF    INF      0 
+Floyd Warshall Algorithm
+We initialize the solution matrix same as the input graph matrix as a first step. Then we update the solution matrix 
+by considering all vertices as an intermediate vertex. The idea is to one by one pick all vertices and update all shortest 
+paths which include the picked vertex as an intermediate vertex in the shortest path. When we pick vertex number k as an 
+intermediate vertex, we already have considered vertices {0, 1, 2, .. k-1} as intermediate vertices. For every pair (i, j) 
+of source and destination vertices respectively, there are two possible cases.
+1) k is not an intermediate vertex in shortest path from i to j. We keep the value of dist[i][j] as it is.
+2) k is an intermediate vertex in shortest path from i to j. We update the value of dist[i][j] as dist[i][k] + dist[k][j].
+
+Time Complexity: O(V^3)
+
+See Images
+ */
+
+package graphs;
+
+/**
+ * Created by poorvank on 11/12/15.
+ */
+public class FloydWarshall {
+    
+    private static final int INF = Integer.MAX_VALUE;
+    
+    public static void main(String[] args) {
+        
+        int[][] graph = new int[][]{ {0,   5,  INF, 10},
+                                     {INF, 0,   3, INF},
+                                     {INF, INF, 0,   1},
+                                     {INF, INF, INF, 0}};
+        
+        printAllShortestPath(graph);
+        
+        
+    }
+    
+    private static void printAllShortestPath(int[][] graph) {
+
+        int V = graph.length;
+        
+        for (int k=0;k<V;k++) {
+            for (int i=0;i<V;i++) {
+                for (int j=0;j<V;j++) {
+                    if(graph[i][k] != INF && graph[k][j] != INF && graph[i][k] + graph[k][j] < graph[i][j]) {
+                        graph[i][j] = graph[i][k] + graph[k][j];
+                    }
+                }
+            }
+        }
+        
+        for (int i=0;i<V;i++) {
+            for (int j=0;j<V;j++) {
+                if(graph[i][j]==INF) {
+                    System.out.print("INF" + " ");
+                }
+                else {
+                    System.out.print(graph[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+        
+        
+    }
+    
+}
+
+
