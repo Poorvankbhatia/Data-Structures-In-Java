c633bc05a669f57efed184bced5cb3951780a880
Poorvankbhatia <puravbhatia9@gmail.com>
2015-12-13 23:48:57 +0530

    Binary Strings

--- src/dp/BinaryStrings.java
+++ src/dp/BinaryStrings.java
@@ -25,25 +25,25 @@ public class BinaryStrings {
 
     public static void main(String[] args) {
 
-        int n = 2;
+        int n = 3;
         count(n);
 
     }
 
     private static void count(int n) {
 
-        int[] a = new int[n];
-        int[] b = new int[n];
+        int[] a = new int[n+1];
+        int[] b = new int[n+1];
 
-        a[0] = 1;
-        b[0] = 1;
+        a[1] = 1;
+        b[1] = 1;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 2; i <=n; i++) {
             a[i] = a[i - 1] + b[i - 1];
             b[i] = a[i - 1];
         }
 
-        System.out.println(a[n - 1] + " " + b[n - 1]);
+        System.out.println(a[n] + " " + b[n]);
 
     }
 
@@ -52,7 +52,7 @@ public class BinaryStrings {
 
 /*
 
-Let a[i] be the number of binary strings of length i+1 which do not contain any two consecutive 1’s and which end in 0.
+Let a[i] be the number of binary strings of length i which do not contain any two consecutive 1’s and which end in 0.
 Similarly, let b[i] be the number of such strings which end in 1. We can append either 0 or 1 to a string ending in 0,
 but we can only append 0 to a string ending in 1. This yields the recurrence relation:
 
