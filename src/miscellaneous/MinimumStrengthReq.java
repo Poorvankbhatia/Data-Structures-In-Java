a48f6462403ffc2e976c3bd07002d995b2758dc2
Poorvankbhatia <puravbhatia9@gmail.com>
2015-12-21 22:12:34 +0530

    min strength req

---
+++ src/miscellaneous/MinimumStrengthReq.java
@@ -0,0 +1,17 @@
+package miscellaneous;
+
+/**
+ * Created by poorvank on 12/19/15.
+ */
+public class MinimumStrengthReq {
+    
+    public static void main(String[] args) {
+        
+        int[][] arr = new int[][] {{ 1,-3,0},
+                                   {-2,1,-1}};
+        
+        
+        
+    }
+    
+}
