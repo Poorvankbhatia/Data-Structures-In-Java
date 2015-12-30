fb326f4f26630e34a256d378a4ba3f09cd75f817
Poorvankbhatia <puravbhatia9@gmail.com>
2015-12-30 23:21:31 +0530

    Remove Duplicates

---
+++ src/strings/RemoveDuplicates.java
@@ -0,0 +1,42 @@
+/*
+Remove duplicates from string
+ */
+
+package strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by poorvank on 12/30/15.
+ */
+public class RemoveDuplicates {
+    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        char[] arr = input.toCharArray();
+        Arrays.sort(arr);
+        
+        input = new String(arr);
+        
+        
+        char ch = input.charAt(0);
+        int count =1,i=1;
+        System.out.print(ch);
+        while (i<input.length()) {
+            
+            while (input.charAt(i)!=ch) {
+                ch = input.charAt(i);
+                System.out.print(ch);
+                count++;
+            }
+            
+            i++;
+        }
+        
+        System.out.println("\n"+count);
+    }
+    
+}
