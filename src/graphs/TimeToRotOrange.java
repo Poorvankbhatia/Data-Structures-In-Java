5b13671d2c030f37936c189faabfc0882d2b1adb
Poorvankbhatia <puravbhatia9@gmail.com>
2016-01-12 21:06:32 +0530

    time to rot refactor

--- src/graphs/TimeToRotOrange.java
+++ src/graphs/TimeToRotOrange.java
@@ -8,31 +8,31 @@ import java.util.Queue;
  */
 
 class Element {
-    
-    public int x,y;
-    
+
+    public int x, y;
+
 }
 
 public class TimeToRotOrange {
-    
+
     public static void main(String[] args) {
-        
-        
-        int[][] arr = new int[][]{ {2, 1, 0, 2, 1},
-                                   {1, 0, 1, 2, 1},
-                                   {1, 0, 0, 2, 1}};
-        
-        
+
+
+        int[][] arr = new int[][]{{2, 1, 0, 2, 1},
+                {1, 0, 1, 2, 1},
+                {1, 0, 0, 2, 1}};
+
+
         System.out.println(minimumTime(arr));
-        
+
     }
-    
-    private static boolean isValid(int x,int y,int row,int col) {
-        
-        return (x>=0 && x<row && y>=0 && y<col);
-        
+
+    private static boolean isValid(int x, int y, int row, int col) {
+
+        return (x >= 0 && x < row && y >= 0 && y < col);
+
     }
-    
+
     private static int minimumTime(int[][] arr) {
 
 
@@ -40,10 +40,10 @@ public class TimeToRotOrange {
         int row = arr.length;
         int col = arr[0].length;
 
-        
-        for(int i=0;i<row;i++) {
-            for (int j=0;j<col;j++) {
-                if(arr[i][j] == 2) {
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (arr[i][j] == 2) {
                     Element element = new Element();
                     element.x = i;
                     element.y = j;
@@ -51,87 +51,87 @@ public class TimeToRotOrange {
                 }
             }
         }
-        
+
         System.out.println(queue.size());
-        
-        int ans =0;
-        
+
+        int ans = 0;
+
         Element delimiter = new Element();
         delimiter.x = -1;
         delimiter.y = -1;
-        
-        
+
+
         queue.add(delimiter);
-        
-        for (int i=0;i<=queue.size();i++) {
+
+        for (int i = 0; i <= queue.size(); i++) {
             System.out.println(queue.peek().x + " " + queue.peek().y);
             queue.remove();
         }
-        
+
         System.exit(0);
-        
+
         while (!queue.isEmpty()) {
 
             boolean flag = false;
-            
-            while (queue.peek().x!=-1 && queue.peek().y!=-1) {
+
+            while (queue.peek().x != -1 && queue.peek().y != -1) {
 
                 Element current = queue.peek();
 
-                if(isValid(current.x+1,current.y,row,col) && arr[current.x+1][current.y]==1) {
+                if (isValid(current.x + 1, current.y, row, col) && arr[current.x + 1][current.y] == 1) {
 
-                    if(!flag) {
+                    if (!flag) {
                         flag = true;
                         System.out.println(1);
                         ans++;
                     }
 
-                    arr[current.x+1][current.y] = 2;
+                    arr[current.x + 1][current.y] = 2;
                     current.x++;
                     queue.add(current);
                     current.x--;
 
                 }
 
-                if(isValid(current.x,current.y+1,row,col) && arr[current.x][current.y+1]==1) {
+                if (isValid(current.x, current.y + 1, row, col) && arr[current.x][current.y + 1] == 1) {
 
-                    if(!flag) {
+                    if (!flag) {
                         flag = true;
                         System.out.println(2);
                         ans++;
                     }
 
-                    arr[current.x][current.y+1] = 2;
+                    arr[current.x][current.y + 1] = 2;
                     current.y++;
                     queue.add(current);
                     current.y--;
 
                 }
 
-                if(isValid(current.x-1,current.y,row,col) && arr[current.x-1][current.y]==1) {
+                if (isValid(current.x - 1, current.y, row, col) && arr[current.x - 1][current.y] == 1) {
 
-                    if(!flag) {
+                    if (!flag) {
                         flag = true;
                         System.out.println(3);
                         ans++;
                     }
 
-                    arr[current.x-1][current.y] = 2;
+                    arr[current.x - 1][current.y] = 2;
                     current.x--;
                     queue.add(current);
                     current.x++;
 
                 }
 
-                if(isValid(current.x,current.y-1,row,col) && arr[current.x][current.y-1]==1) {
+                if (isValid(current.x, current.y - 1, row, col) && arr[current.x][current.y - 1] == 1) {
 
-                    if(!flag) {
+                    if (!flag) {
                         flag = true;
                         System.out.println(4);
                         ans++;
                     }
 
-                    arr[current.x+1][current.y] = 2;
+                    arr[current.x + 1][current.y] = 2;
                     current.y--;
                     queue.add(current);
                     current.y++;
@@ -145,20 +145,19 @@ public class TimeToRotOrange {
 
             queue.remove();
 
-            if(!queue.isEmpty()) {
+            if (!queue.isEmpty()) {
                 delimiter.x = -1;
                 delimiter.y = -1;
 
                 queue.add(delimiter);
             }
-            
-            
+
+
         }
-        
-        
-        
+
+
         return ans;
-        
+
     }
-    
+
 }
