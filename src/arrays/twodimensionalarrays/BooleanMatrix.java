825299b5725fe923026a7b986e09e5efde5b886a
Poorvankbhatia <puravbhatia9@gmail.com>
2016-01-19 11:46:11 +0530

    Refactor

--- src/arrays/twodimensionalarrays/BooleanMatrix.java
+++ src/arrays/twodimensionalarrays/BooleanMatrix.java
@@ -55,10 +55,11 @@ public class BooleanMatrix {
         
         /*
         
-        1) Create two temporary arrays row[M] and col[N]. Initialize all values of row[] and col[] as 0.
+1) Create two temporary arrays row[M] and col[N]. Initialize all values of row[] and col[] as 0.
 2) Traverse the input matrix mat[M][N]. If you see an entry mat[i][j] as true, then mark row[i] and col[j] as true.
 3) Traverse the input matrix mat[M][N] again. For each entry mat[i][j], 
-check the values of row[i] and col[j]. If any of the two values (row[i] or col[j]) is true, then mark mat[i][j] as true.
+check the values of row[i] and col[j]. If any of the two values (row[i] or col[j]) is true, then mark mat[i][j] as
+ true.
         
          */
         int[] rowArr = new int[row];
