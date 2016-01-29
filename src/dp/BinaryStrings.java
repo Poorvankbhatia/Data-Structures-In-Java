22e135ca6a518610e27cf3b5bb24b810b4e77130
Poorvankbhatia <puravbhatia9@gmail.com>
2016-01-29 14:35:17 +0530

    Binary Strings

--- src/dp/BinaryStrings.java
+++ src/dp/BinaryStrings.java
@@ -32,13 +32,13 @@ public class BinaryStrings {
 
     private static void count(int n) {
 
-        int[] a = new int[n+1];
-        int[] b = new int[n+1];
+        int[] a = new int[n + 1];
+        int[] b = new int[n + 1];
 
         a[1] = 1;
         b[1] = 1;
 
-        for (int i = 2; i <=n; i++) {
+        for (int i = 2; i <= n; i++) {
             a[i] = a[i - 1] + b[i - 1];
             b[i] = a[i - 1];
         }
