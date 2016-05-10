5d2ced7d5b9ccebbf45f661b382aeb6796e936eb
Poorvankbhatia <puravbhatia9@gmail.com>
2016-05-11 01:25:17 +0530

    Refactored : Topological Sort Modified

--- src/graphs/TopologicalSort.java
+++ src/graphs/TopologicalSort.java
@@ -1,5 +1,10 @@
 /*
 
+Given a digraph, put the vertices in
+order such that all its directed edges point from a vertex
+earlier in the order to a vertex later in the order (or
+report that doing so is not possible).
+
 Topological sorting for Directed Acyclic Graph (DAG) is a linear ordering of vertices such that for every directed edge uv,
  vertex u comes before v in the ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
 
@@ -17,51 +22,41 @@ for 6 vertices and edges as
 
 package graphs;
 
+import utility.Digraph;
+import utility.Stack;
+
 /**
  * Created by poorvank on 4/7/15.
  */
 public class TopologicalSort {
 
+    private Iterable<Integer> order;
+
     public static void main(String[] args) {
 
-        Vertex[] vArray = Input.graphInput();
-        boolean[] visited = new boolean[vArray.length];
 
-        sort(vArray, visited);
+        TopologicalSort ts = new TopologicalSort();
+        Digraph digraph = new Digraph(6);
+        digraph.addEdge(5,0);
+        digraph.addEdge(5,2);
+        digraph.addEdge(4,0);
+        digraph.addEdge(4,1);
+        digraph.addEdge(2,3);
+        digraph.addEdge(3,1);
+
+
+        if(!DetectCycleGraph.isCyclic(digraph)) {
+            DepthFirstOrders depthFirstOrders = new DepthFirstOrders(digraph);
+            ts.order = depthFirstOrders.getReversePost();
+        }
+
+        for (Integer v : ts.order) {
+            System.out.print(v + " ");
+        }
 
 
     }
 
-
-    private static void sort(Vertex[] vArray, boolean[] visited) {
-
-        Stack stack = new Stack();
-
-        for (int i = 0; i < vArray.length; i++) {
-            if (!visited[i]) {
-                sortUtil(vArray, visited, stack, i);
-            }
-        }
-
-        while (!stack.isEmpty()) {
-            System.out.print(" " + ((Vertex) stack.pop()).info);
-        }
-
-    }
-
-
-    private static void sortUtil(Vertex[] vArray, boolean[] visited, Stack stack, int i) {
-
-        visited[i] = true;
-
-        for (int v : vArray[i].adjacentVertices) {
-            if (!visited[v]) {
-                sortUtil(vArray, visited, stack, v);
-            }
-        }
-
-        stack.push(vArray[i]);
-    }
 
 }
 
@@ -69,9 +64,21 @@ public class TopologicalSort {
 /*
 
 In topological sorting, we use a temporary stack. We don’t print the vertexToConsider immediately, we first recursively call
- topological sorting for all its adjacent vertices, then push it to a stack.
-  Finally, print contents of stack. Note that a vertexToConsider is pushed to stack only
-  when all of its adjacent vertices (and their adjacent vertices and so on) are already in stack.
+topological sorting for all its adjacent vertices, then push it to a stack.
+Finally, print contents of stack. Note that a vertexToConsider is pushed to stack only
+when all of its adjacent vertices (and their adjacent vertices and so on) are already in stack.
+
+Cycles in digraphs. If job x must be completed before job y, job y before job z, and
+job z before job x, then someone has made a mistake, because those three constraints
+cannot all be satisfied. In general, if a precedence-constrained scheduling problem has
+a directed cycle, then there is no feasible solution. To check for such errors, we need to
+be able to solve the following problem:
+
+Proposition E. A digraph has a topological order if and only if it is a DAG.
+Proof: If the digraph has a directed cycle, it has no topological order. Conversely,
+the algorithm that we are about to examine computes a topological order for any
+given DAG.
+
 
 
  */
