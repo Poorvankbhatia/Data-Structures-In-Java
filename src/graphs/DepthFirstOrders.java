4add9eda2a625f9f7436e96da3222ab05d3a9572
Poorvankbhatia <puravbhatia9@gmail.com>
2016-05-11 01:24:45 +0530

    Differnt orders added

--- src/graphs/DepthFirstOrders.java
+++ src/graphs/DepthFirstOrders.java
@@ -1,7 +1,47 @@
 package graphs;
 
+import utility.*;
+import utility.Stack;
+
 /**
- * Created by poorvank.b on 10/05/16.
+ * Created by poorvank on 10/05/16.
  */
 public class DepthFirstOrders {
+
+    private boolean[] marked;
+
+    private Stack<Integer> reversePost;
+    private Queue<Integer> post;
+
+    public DepthFirstOrders(Digraph G) {
+        int size = G.getVertexCount();
+        marked = new boolean[size];
+        reversePost = new Stack<>();
+        post = new Queue<>();
+        for (int i=0;i<size;i++) {
+            if(!marked[i]) {
+                dfs(G,i);
+            }
+        }
+
+    }
+
+    private void dfs(Digraph G,int i) {
+        marked[i] = true;
+        for (Integer vertex: G.getAdj(i)) {
+            if(!marked[vertex]) {
+                dfs(G,vertex);
+            }
+        }
+        reversePost.push(i);
+        post.enqueue(i);
+    }
+
+    public Iterable<Integer> getReversePost() {
+        return reversePost;
+    }
+
+    public Iterable<Integer> getPost() {
+        return post;
+    }
 }
