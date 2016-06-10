49b2b61511cc18bead2c9adf699dab6e3de81682
Poorvankbhatia <puravbhatia9@gmail.com>
2016-06-11 00:06:50 +0530

    Completely Refactored with new priority Queue implementation : Merge k Sorted Arrays

--- src/trees/heap/MergeKSortedArrays.java
+++ src/trees/heap/MergeKSortedArrays.java
@@ -16,132 +16,84 @@ Output: 0 1 2 3 4 5 6 7 8 9 10 11
 
 package trees.heap;
 
+import utility.priorityQueueClasses.MinPriorityQueue;
+
 import java.util.Arrays;
 
 /**
  * Created by poorvank on 4/22/15.
  */
 
-class MinHeapNode {
-
-    int element;
-    int arrayIndex;
-    int nextIndex;
-
-}
-
-
-class MinHeap {
-
-    int heapSize;
-    MinHeapNode[] minHeapNodes;
-
-    public MinHeap(int k) {
-
-        heapSize = k;
-        minHeapNodes = new MinHeapNode[heapSize];
-
-    }
-
-    public int[] mergeKArrays(int[][] arr, int k, int n) {
-
-        int[] output = new int[n * k];
-
-        for (int i = 0; i < k; i++) {
-
-            minHeapNodes[i] = new MinHeapNode();
-            minHeapNodes[i].element = arr[i][0];// Store the first element
-            minHeapNodes[i].arrayIndex = i;// index of array
-            minHeapNodes[i].nextIndex = 1;// Index of next element to be stored from array
-
-        }
-
-        minHeap(minHeapNodes);
-
-
-        for (int count = 0; count < (n * k); count++) {
-
-            MinHeapNode minHeapNode = minHeapNodes[0];
-            output[count] = minHeapNode.element;
-
-            if (minHeapNode.nextIndex < n) {
-                minHeapNode.element = arr[minHeapNode.arrayIndex][minHeapNode.nextIndex];
-                minHeapNode.nextIndex += 1;
-            } else {
-                minHeapNode.element = Integer.MAX_VALUE;
-            }
-
-            minHeap(minHeapNodes);
-        }
-
-        return output;
-
-    }
-
-    public void minHeap(MinHeapNode[] minHeapNodes) {
-
-        for (int i = minHeapNodes.length / 2; i >= 0; i--) {
-            restoreDown(minHeapNodes, i);
-        }
-
-    }
-
-    /**
-     * @param minHeapNodes
-     * @param i
-     */
-    private void restoreDown(MinHeapNode[] minHeapNodes, int i) {
-
-        int left = (2 * i) + 1;
-        int right = (2 * i) + 2;
-        MinHeapNode num = minHeapNodes[i];
-
-        while (right <= minHeapNodes.length - 1) {
-
-            if (num.element <= minHeapNodes[right].element && num.element <= minHeapNodes[left].element) {
-                minHeapNodes[i] = num;
-                return;
-            } else if (minHeapNodes[right].element < minHeapNodes[left].element) {
-                minHeapNodes[i] = minHeapNodes[right];
-                i = right;
-            } else {
-                minHeapNodes[i] = minHeapNodes[left];
-                i = left;
-            }
-
-            left = (2 * i) + 1;
-            right = (2 * i) + 2;
-
-        }
-
-        if (left == minHeapNodes.length - 1 && minHeapNodes[left].element < num.element) {
-            minHeapNodes[i] = minHeapNodes[left];
-            i = left;
-        }
-
-        minHeapNodes[i] = num;
-    }
-
-}
-
-
 public class MergeKSortedArrays {
+
+    private static MinPriorityQueue<MinHeapNode> priorityQueue;
+
+    public MergeKSortedArrays(int noOfArrays) {
+        priorityQueue = new MinPriorityQueue<>(noOfArrays);
+    }
+
+    private static class MinHeapNode implements Comparable<MinHeapNode> {
+        private Integer info;           //value of the element
+        private Integer arrayIndex;     //Which array it the above element belongs to
+        private Integer nextIndex;      //What is the next Index to be encountered
+
+
+        @Override
+        public int compareTo(MinHeapNode h) {
+            if(this.info > h.info) {
+                return 1;
+            } else if(this.info < h.info) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
 
-        int[][] arr = new int[][]{{2, 6, 12, 34},
-                {1, 9, 20, 1000},
-                {23, 34, 90, 2000}};
+        int arr[][] = { {1, 3, 5, 7},
+                        {2, 4, 6, 8},
+                        {0, 9, 10, 11}} ;
 
-        int k = arr.length;
-        int n = arr[0].length;
+        int row = arr.length;
+        int col = arr[0].length;
+        MergeKSortedArrays merge = new MergeKSortedArrays(row);
 
-        MinHeap minHeap = new MinHeap(k);
-        System.out.println(Arrays.toString(minHeap.mergeKArrays(arr, k, n)));
+        for (int i=0;i<row;i++) {
+            MinHeapNode minHeapNode = new MergeKSortedArrays.MinHeapNode();
+            minHeapNode.info = arr[i][0];
+            minHeapNode.arrayIndex = i;
+            minHeapNode.nextIndex = 1;
+            priorityQueue.insert(minHeapNode);
+        }
+
+        int[] output = new int[(row*col)];
+        for (int i=0;i<output.length;i++) {
+
+            MinHeapNode current = priorityQueue.delMin();
+            output[i] = current.info;
+            MinHeapNode node = new MinHeapNode();
+            if(current.nextIndex < col) {
+                node.nextIndex = current.nextIndex + 1;
+                node.info = arr[current.arrayIndex][current.nextIndex];
+                node.arrayIndex = current.arrayIndex;
+            } else {
+                node.info = Integer.MAX_VALUE;
+                node.arrayIndex = current.arrayIndex;
+                node.nextIndex = current.nextIndex + 1;
+            }
+
+            priorityQueue.insert(node);
+
+        }
+
+        System.out.println(Arrays.toString(output));
+
     }
 
 }
-
 
 /*
 
