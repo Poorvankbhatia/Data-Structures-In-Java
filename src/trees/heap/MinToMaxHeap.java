f3aea4f354ac1f81d20c1a454546da7a9f8adb06
Poorvankbhatia <puravbhatia9@gmail.com>
2016-06-16 09:40:00 +0530

    Min to Max

---
+++ src/trees/heap/MinToMaxHeap.java
@@ -0,0 +1,18 @@
+package trees.heap;
+
+import utility.priorityQueueClasses.MaxPriorityQueue;
+
+/**
+ * Created by poorvank.b on 11/06/16.
+ */
+public class MinToMaxHeap {
+
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[]{3,5,9,6,8,20,10,12,18,9};
+        MaxPriorityQueue<Integer> pq = new MaxPriorityQueue<>(arr);
+        for (Integer element : pq) {
+            System.out.print(element + " ");
+        }
+    }
+
+}
