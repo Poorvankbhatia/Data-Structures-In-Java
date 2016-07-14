2ea93998d2e5c9f717ad7358d175110e8cf179ef
Poorvankbhatia <puravbhatia9@gmail.com>
2016-07-15 00:33:51 +0530

    lcp implementation added

---
+++ src/utility/LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+package utility;
+
+/**
+ * Created by poorvank.b on 14/07/16.
+ */
+public class LongestCommonPrefix {
+
+    public static  String value(String s1,String s2) {
+        int N = Math.min(s1.length(),s2.length());
+        for (int i=0;i<N;i++) {
+            if(s1.charAt(i)!=s2.charAt(i)) {
+                return s1.substring(0,i);
+            }
+        }
+        return s1.substring(0,N);
+    }
+
+}
