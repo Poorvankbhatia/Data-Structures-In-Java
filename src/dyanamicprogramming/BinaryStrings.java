cf0685f139d08d295b4fc6e613450b2c94f0b937
Poorvankbhatia <puravbhatia9@gmail.com>
2016-10-24 19:51:25 +0530

    Refactor Binary Strings

--- src/dyanamicprogramming/BinaryStrings.java
+++ src/dyanamicprogramming/BinaryStrings.java
@@ -25,25 +25,34 @@ public class BinaryStrings {
 
     public static void main(String[] args) {
 
-        int n = 3;
-        count(n);
+        int n = 2;
+        System.out.print(count(n));
 
     }
 
-    private static void count(int n) {
+    private static int count(int n) {
+
+        if(n==0) {
+            return n;
+        }
+        if(n==1) {
+            return 2;
+        }
 
         int[] a = new int[n + 1];
         int[] b = new int[n + 1];
 
-        a[1] = 1;
-        b[1] = 1;
+        a[2] = 1;
+        b[2] = 1;
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 3; i <= n; i++) {
             a[i] = a[i - 1] + b[i - 1];
             b[i] = a[i - 1];
         }
 
         System.out.println(a[n] + " " + b[n]);
+
+        return a[n]+b[n];
 
     }
 
