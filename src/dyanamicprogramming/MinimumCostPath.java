868200aa5bd238559bcf783c5da4dac008ced475
Poorvankbhatia <puravbhatia9@gmail.com>
2016-12-03 19:31:59 +0530

    Minimum Cost path recursive function corrected & question added

--- src/dyanamicprogramming/MinimumCostPath.java
+++ src/dyanamicprogramming/MinimumCostPath.java
@@ -1,3 +1,18 @@
+/*
+
+Given a cost matrix cost[][] and a position (m, n) in cost[][], write a function that returns cost of minimum cost path to reach (m, n) from (0, 0).
+Each cell of the matrix represents a cost to traverse through that cell. Total cost of a path to reach (m, n) is sum of all the costs on that path
+(including both source and destination). You can only traverse down, right and diagonally lower cells from a given cell, i.e.,
+ from a given cell (i, j), cells (i+1, j), (i, j+1) and (i+1, j+1) can be traversed. You may assume that all costs are positive integers.
+
+ The path with minimum cost in the following figure: The path is (0, 0) –> (0, 1) –> (1, 2) –> (2, 2).
+  The cost of the path is 8 (1 + 2 + 2 + 3).
+
+                                   {{1, 2, 3},
+                                   {4, 8, 2},
+                                   {1, 5, 3}};
+
+ */
 package dyanamicprogramming;
 
 /**
@@ -11,8 +26,8 @@ public class MinimumCostPath {
     public static void main(String[] args) {
 
         int[][] cost = new int[][]{{1, 2, 3},
-                {4, 8, 2},
-                {1, 5, 3}};
+                                   {4, 8, 2},
+                                   {1, 5, 3}};
 
         System.out.println(minCostRecursive(cost, 2, 2));
         System.out.println(minCostDP(cost, 2, 2));
@@ -29,7 +44,7 @@ public class MinimumCostPath {
             return cost[row][col];
         }
 
-        return cost[row][col] + minOfThree(cost[row][col - 1], cost[row - 1][col - 1], cost[row - 1][col - 1]);
+        return cost[row][col] + minOfThree(minCostRecursive(cost,row,col-1), minCostRecursive(cost,row-1,col), minCostRecursive(cost,row-1,col-1));
 
     }
 
@@ -39,25 +54,25 @@ public class MinimumCostPath {
             return Integer.MAX_VALUE;
         }
 
-        int table[][] = new int[ROW][COL];
+        int dpCost[][] = new int[ROW][COL];
 
-        table[0][0] = cost[0][0];
+        dpCost[0][0] = cost[0][0];
 
         for (int i = 1; i < ROW; i++) {
-            table[i][0] = table[i - 1][0] + cost[i][0];
+            dpCost[i][0] = dpCost[i - 1][0] + cost[i][0];
         }
 
         for (int j = 1; j < COL; j++) {
-            table[0][j] = table[0][j - 1] + cost[0][j];
+            dpCost[0][j] = dpCost[0][j - 1] + cost[0][j];
         }
 
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= col; j++) {
-                table[i][j] = cost[i][j] + minOfThree(table[i - 1][j], table[i - 1][j - 1], table[i][j - 1]);
+                dpCost[i][j] = cost[i][j] + minOfThree(dpCost[i - 1][j], dpCost[i - 1][j - 1], dpCost[i][j - 1]);
             }
         }
 
-        return table[row][col];
+        return dpCost[row][col];
     }
 
     private static int minOfThree(int a, int b, int c) {
