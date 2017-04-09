862404cf253c6d72544e467ad284b8fc0243815d
Poorvankbhatia <puravbhatia9@gmail.com>
2017-04-09 17:02:20 +0530

    Corrected

--- src/arrays/NextGreatestNumber.java
+++ src/arrays/NextGreatestNumber.java
@@ -17,32 +17,32 @@ public class NextGreatestNumber {
 
     private static void printNextNo(String s) {
 
-        int i;
+        int i,len=s.length();
 
-        for (i = s.length() - 2; i >= 0; i--) {
-            if (Character.getNumericValue(s.charAt(i)) < Character.getNumericValue(s.charAt(i + 1))) {
+        for (i = s.length() - 1; i >= 0; i--) {
+            if (Character.getNumericValue(s.charAt(i)) > Character.getNumericValue(s.charAt(i - 1))) {
                 break;
             }
-            if (i == 0) {
-                System.out.println("Not possible");
-                return;
-            }
+        }
+
+        if (i == 0) {
+            System.out.println("Not possible");
+            return;
         }
 
         System.out.println(i);
 
-        int minIndex = i + 1;
+        int minIndex = i;
+        int x = Character.getNumericValue(s.charAt(i-1));
 
-        for (int j = i + 2; j < s.length(); j++) {
-            if (Character.getNumericValue(s.charAt(j)) < Character.getNumericValue(s.charAt(minIndex)) && Character.getNumericValue(s.charAt(minIndex)) > Character.getNumericValue(s.charAt(i))) {
+        for (int j = i+1; j < len; j++)
+            if (Character.getNumericValue(s.charAt(j)) > x && Character.getNumericValue(s.charAt(j)) < Character.getNumericValue(s.charAt(minIndex)))
                 minIndex = j;
-            }
-        }
 
-        String replacedString = replacedString(s, i, minIndex);
+        String replacedString = replacedString(s, i-1, minIndex);
 
         char[] chars = replacedString.toCharArray();
-        Arrays.sort(chars, i + 1, s.length());
+        Arrays.sort(chars, i, s.length());
 
         System.out.println(String.copyValueOf(chars));
 
