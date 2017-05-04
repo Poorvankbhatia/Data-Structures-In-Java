a5c7d1c471aadc7d60a5a46a19c5baf96b3e73dd
Poorvankbhatia <puravbhatia9@gmail.com>
2017-05-04 17:48:16 +0530

    SumArray Closest to k comments added

--- src/arrays/SubarraySumCloseK.java
+++ src/arrays/SubarraySumCloseK.java
@@ -21,6 +21,10 @@ public class SubarraySumCloseK {
         for(int i=0; i<arr.length; i++){
             sum=sum+arr[i];
 
+            /*
+            When you are processing cum[j] what you need to retrieve from the set is the smallest number in the set
+            such which is bigger than cum[j]−k.
+             */
             Integer ceiling = set.ceiling(sum-k);
             if(ceiling!=null){
                 result = Math.max(result, sum-ceiling);
@@ -38,3 +42,18 @@ public class SubarraySumCloseK {
     }
 
 }
+
+
+/*
+
+You can do this in O(nlog(n))
+
+First thing to note is that sum of subarray (i,j] is just the sum of the first j elements less the sum of the first i elements.
+Store these cumulative sums in the array cum. Then the problem reduces to finding  i,j such that i<j and cum[j]−cum[i]
+is as close to kk but lower than it.
+
+To solve this, scan from left to right. Put the cum[i]cum[i] values that you have encountered till now into a set.
+When you are processing cum[j] what you need to retrieve from the set is the smallest number in the set such which is bigger than
+cum[j]−k. This lookup can be done in O(logn) using upper_bound. Hence the overall complexity is O(nlog(n)).
+
+ */
